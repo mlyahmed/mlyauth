94b5a70132bd90296aee1db10c58e6837c2559e3
Moulay Ahmed <ahmed.elidrissi.attach@gmail.com>
2017-12-23 20:56:45 +0100

    init

--- mlyauth-backend/src/test/java/com/mlyauth/itests/api/PersonControllerIT.java
+++ mlyauth-backend/src/test/java/com/mlyauth/itests/api/PersonControllerIT.java
@@ -1,6 +1,5 @@
 package com.mlyauth.itests.api;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mlyauth.beans.PersonBean;
 import com.mlyauth.dao.PersonDAO;
 import com.mlyauth.domain.Person;
@@ -9,15 +8,19 @@ import com.tngtech.java.junit.dataprovider.DataProvider;
 import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.restdocs.payload.PayloadDocumentation;
+import org.springframework.restdocs.payload.RequestFieldsSnippet;
+import org.springframework.restdocs.payload.ResponseFieldsSnippet;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -25,10 +28,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 public class PersonControllerIT extends AbstractIntegrationTest {
 
-    final ObjectMapper mapper = new ObjectMapper();
-
-    @Autowired
-    private MockMvc mockMvc;
+    public static final String ROOT_USERNAME = "root";
+    public static final String ROOT_PASSWORD = "root";
 
     @Autowired
     private PersonDAO personDAO;
@@ -49,32 +50,52 @@ public class PersonControllerIT extends AbstractIntegrationTest {
 
     @Test
     @UseDataProvider("properties")
-    public void when_create_a_new_person_then_create_him(String firstname, String lastname, String username, String email, String password) throws Exception {
-        PersonBean personBean = given_person(firstname, lastname, username, email, password);
+    public void when_create_a_new_person_then_create_him(String... properties) throws Exception {
+        PersonBean personBean = given_person(properties);
         final ResultActions resultActions = when_create_new_person(personBean);
-        then_he_is_well_created(firstname, lastname, username, email, password, resultActions);
+        then_he_is_well_created(resultActions, properties);
+
+        final RequestFieldsSnippet requestDescription = PayloadDocumentation.relaxedRequestFields(
+                fieldWithPath("firstname").type(JsonFieldType.STRING).description("Firstname"),
+                fieldWithPath("lastname").type(JsonFieldType.STRING).description("Lastname"),
+                fieldWithPath("username").type(JsonFieldType.STRING).description("Username"),
+                fieldWithPath("email").type(JsonFieldType.STRING).description("email"),
+                fieldWithPath("password").type(JsonFieldType.STRING).description("password")
+        );
+
+        final ResponseFieldsSnippet responseDescription = PayloadDocumentation.relaxedResponseFields(
+                fieldWithPath("id").type(JsonFieldType.NUMBER).description("id"),
+                fieldWithPath("firstname").type(JsonFieldType.STRING).description("Firstname"),
+                fieldWithPath("lastname").type(JsonFieldType.STRING).description("Lastname"),
+                fieldWithPath("username").type(JsonFieldType.STRING).description("Username"),
+                fieldWithPath("email").type(JsonFieldType.STRING).description("email"),
+                fieldWithPath("password").type(JsonFieldType.STRING).description("password")
+        );
+
+        resultActions.andDo(document("person-creation-ok", requestDescription, responseDescription));
+
     }
 
-    private void then_he_is_well_created(String firstname, String lastname, String username, String email, String password, ResultActions resultActions) throws Exception {
-        resultActions.andExpect(status().is(HttpStatus.CREATED.value()));
-        final Person person = personDAO.findByUsername(username);
+    private void then_he_is_well_created(ResultActions resultActions, String... properties) throws Exception {
+        resultActions.andExpect(status().isCreated());
+        final Person person = personDAO.findByUsername(properties[2]);
         assertThat(person, notNullValue());
         assertThat(person.getId(), notNullValue());
-        assertThat(person.getFirstname(), equalTo(firstname));
-        assertThat(person.getLastname(), equalTo(lastname));
-        assertThat(person.getUsername(), equalTo(username));
-        assertThat(person.getEmail(), equalTo(email));
-        assertThat(person.getPassword(), not(equalTo(password)));
-        assertTrue(passwordEncoder.matches(password, person.getPassword()));
+        assertThat(person.getFirstname(), equalTo(properties[0]));
+        assertThat(person.getLastname(), equalTo(properties[1]));
+        assertThat(person.getUsername(), equalTo(properties[2]));
+        assertThat(person.getEmail(), equalTo(properties[3]));
+        assertThat(person.getPassword(), not(equalTo(properties[4])));
+        assertTrue(passwordEncoder.matches(properties[4], person.getPassword()));
     }
 
-    private PersonBean given_person(String firstname, String lastname, String username, String email, String password) {
+    private PersonBean given_person(String... properties) {
         PersonBean personBean = new PersonBean();
-        personBean.setFirstname(firstname);
-        personBean.setLastname(lastname);
-        personBean.setUsername(username);
-        personBean.setEmail(email);
-        personBean.setPassword(password.toCharArray());
+        personBean.setFirstname(properties[0]);
+        personBean.setLastname(properties[1]);
+        personBean.setUsername(properties[2]);
+        personBean.setEmail(properties[3]);
+        personBean.setPassword(properties[4].toCharArray());
         return personBean;
     }
 
@@ -83,7 +104,7 @@ public class PersonControllerIT extends AbstractIntegrationTest {
     public void when_create_a_new_person_and_already_exists_then_error() throws Exception {
         PersonBean personBean = given_person("Moulay", "ATTACH", "mlyattach", "moulay.attach@gmail.com", "password");
         final ResultActions resultActions = when_create_new_person(personBean);
-        resultActions.andExpect(status().is(HttpStatus.BAD_REQUEST.value()))
+        resultActions.andExpect(status().isBadRequest())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(jsonPath("$", hasSize(1)))
                 .andExpect(jsonPath("$.[0].code", equalTo("PERSON_ALREADY_EXISTS")));
@@ -92,7 +113,7 @@ public class PersonControllerIT extends AbstractIntegrationTest {
     private ResultActions when_create_new_person(PersonBean personBean) throws Exception {
         return mockMvc.perform(post("/domain/person")
                 .content(mapper.writeValueAsString(personBean))
-                .with(httpBasic("root", "root"))
+                .with(httpBasic(ROOT_USERNAME, ROOT_PASSWORD))
                 .contentType(MediaType.APPLICATION_JSON)
                 .characterEncoding("UTF-8"));
     }
