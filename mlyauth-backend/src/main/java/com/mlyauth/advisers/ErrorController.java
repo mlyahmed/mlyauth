ec8732edf11eda195963b1185aa7313d25e4386f
Moulay Ahmed <ahmed.elidrissi.attach@gmail.com>
2018-02-17 21:46:32 +0100

    init

--- mlyauth-backend/src/main/java/com/mlyauth/advisers/ErrorController.java
+++ mlyauth-backend/src/main/java/com/mlyauth/advisers/ErrorController.java
@@ -1,13 +1,18 @@
 package com.mlyauth.advisers;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
-public class ErrorController {
+public class ErrorController extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Throwable.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
@@ -17,4 +22,18 @@ public class ErrorController {
         return "error";
     }
 
+    @ResponseBody
+    public ResponseEntity handle(HttpServletRequest request, Throwable throwable, final Model model) {
+        model.addAttribute("errorMessage", throwable.getMessage());
+        model.addAttribute("MLY_AUTH_ERROR_CODE", "APP_NOT_ASSIGNED");
+        return ResponseEntity.status(getStatus(request)).build();
+    }
+
+    private HttpStatus getStatus(HttpServletRequest request) {
+        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        if (statusCode == null) {
+            return HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        return HttpStatus.valueOf(statusCode);
+    }
 }
