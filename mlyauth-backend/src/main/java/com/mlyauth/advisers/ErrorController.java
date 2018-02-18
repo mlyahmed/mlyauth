9351494b00bb053b240b5fd61fc2dba01cf33c7d
Moulay Ahmed <ahmed.elidrissi.attach@gmail.com>
2018-02-18 03:22:25 +0100

    init

--- mlyauth-backend/src/main/java/com/mlyauth/advisers/ErrorController.java
+++ mlyauth-backend/src/main/java/com/mlyauth/advisers/ErrorController.java
@@ -2,11 +2,8 @@ package com.mlyauth.advisers;
 
 import com.mlyauth.exception.IDPException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -22,13 +19,6 @@ public class ErrorController extends ResponseEntityExceptionHandler {
         final ModelAndView view = new ModelAndView("error/" + getStatus(request).value());
         view.setStatus(getStatus(request));
         return view;
-    }
-
-    @ResponseBody
-    public ResponseEntity handle(HttpServletRequest request, IDPException error, Model model) {
-        model.addAttribute("errorMessage", error.getMessage());
-        model.addAttribute("MLY_AUTH_ERROR_CODE", "APP_NOT_ASSIGNED");
-        return ResponseEntity.status(getStatus(request)).build();
     }
 
     private HttpStatus getStatus(HttpServletRequest request) {
