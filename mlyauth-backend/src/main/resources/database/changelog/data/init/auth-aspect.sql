8fcaa41b6cc438103daf53d86d1f82a44053609e
Moulay Ahmed <ahmed.elidrissi.attach@gmail.com>
2018-03-04 15:40:13 +0100

    init

--- mlyauth-backend/src/main/resources/database/changelog/data/init/auth-aspect.sql
+++ mlyauth-backend/src/main/resources/database/changelog/data/init/auth-aspect.sql
@@ -5,5 +5,7 @@ INSERT INTO AUTH_ASPECT (TYPE, TITLE, DESCRIPTION)
 VALUES ('SP_BASIC', 'Basic AUthentication', 'Http Basic AUthentication');
 INSERT INTO AUTH_ASPECT (TYPE, TITLE, DESCRIPTION)
 VALUES ('SP_SAML', 'SAML SP Authentication', 'SAML SP Authentication');
+INSERT INTO AUTH_ASPECT (TYPE, TITLE, DESCRIPTION)
+VALUES ('IDP_JOSE', 'SAML IDP Authentication', 'SAML IDP Authentication');
 INSERT INTO AUTH_ASPECT (TYPE, TITLE, DESCRIPTION) VALUES ('UNDEFINED', 'Unknown Authentication Aspect', 'Unknown Authentication Aspect');
 --rollback DELETE FROM AUTH_ASPECT;
