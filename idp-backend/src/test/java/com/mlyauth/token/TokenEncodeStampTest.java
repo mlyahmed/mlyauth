7019e19db2e6727a5e185a052ce1aab33d64072c
Moulay Ahmed <ahmed.elidrissi.attach@gmail.com>
2018-03-18 16:05:57 +0100

    open connect id

--- idp-backend/src/test/java/com/mlyauth/token/TokenEncodeStampTest.java
+++ idp-backend/src/test/java/com/mlyauth/token/TokenEncodeStampTest.java
@@ -4,8 +4,6 @@ import com.mlyauth.SecurityConfig;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.Test;
 
-import java.util.UUID;
-
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
@@ -14,7 +12,7 @@ public class TokenEncodeStampTest {
 
     @Test
     public void generate_new_stamps() {
-        final String stamp = UUID.randomUUID().toString();
+        final String stamp = "Br5NUEuDdQpyMTs"; //UUID.randomUUID().toString();
         SecurityConfig securityConfig = new SecurityConfig();
         final String encodedStamp = securityConfig.passwordEncoder().encode(stamp);
         final String checksum = DigestUtils.sha256Hex(encodedStamp);
