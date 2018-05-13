d7bb3900764210f16328a739931e97112d2093fe
Moulay Ahmed <ahmed.elidrissi.attach@gmail.com>
2018-05-13 17:47:27 +0200

    To Prima-solutions

--- idp-backend/src/main/resources/database/changelog/data/init/navigations/001-auto-navigation.sql
+++ idp-backend/src/main/resources/database/changelog/data/init/navigations/001-auto-navigation.sql
@@ -1,3 +1,4 @@
 --liquibase formatted sql
---changeset mlyahmed:1 runOnChange:true
+
+--changeset ahmed.elidrissi:1 runOnChange:true
 INSERT INTO AUTO_NAVIGATION (ID, ROLE, APPLICATION_TYPE) VALUES (1, 'CLIENT', 'POLICY');
