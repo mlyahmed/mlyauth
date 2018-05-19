7e5c00d9cf9a5ee137bed2a1c3567cfa3831e45e
Moulay Ahmed <ahmed.elidrissi.attach@gmail.com>
2018-05-19 05:28:09 +0200

    To Prima-solutions

--- idp-backend/src/test/java/com/primasolutions/idp/person/PersonValidatorTest.java
+++ idp-backend/src/test/java/com/primasolutions/idp/person/PersonValidatorTest.java
@@ -3,13 +3,14 @@ package com.primasolutions.idp.person;
 import com.primasolutions.idp.constants.RoleCode;
 import com.primasolutions.idp.exception.IDPException;
 import com.primasolutions.idp.person.mocks.MockPersonLookuper;
-import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 
+import static com.primasolutions.idp.tools.RandomForTests.randomEmail;
 import static com.primasolutions.idp.tools.RandomForTests.randomName;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.test.util.ReflectionTestUtils.setField;
@@ -35,7 +36,7 @@ public class PersonValidatorTest {
     @Test
     public void when_validate_new_person_and_null_then_error() {
         final IDPException ex = assertThrows(IDPException.class, () -> validator.validateNew(null));
-        assertThat(ex, Matchers.notNullValue());
+        assertThat(ex, notNullValue());
         assertThat(ex.getErrors(), hasSize(1));
         assertThat(ex.getErrors().stream().findFirst().get().getCode(), equalTo("PERSON_IS_NULL"));
     }
@@ -45,7 +46,7 @@ public class PersonValidatorTest {
         given_valid_person_to_create();
         given_the_person_email_is_null();
         final IDPException ex = assertThrows(IDPException.class, () -> validator.validateNew(person));
-        assertThat(ex, Matchers.notNullValue());
+        assertThat(ex, notNullValue());
         assertThat(ex.getErrors(), hasSize(1));
         assertThat(ex.getErrors().stream().findFirst().get().getCode(), equalTo("PERSON_EMAIL_IS_NULL"));
 
@@ -56,7 +57,7 @@ public class PersonValidatorTest {
                 .setFirstname(randomName().getFirstName())
                 .setLastname(randomName().getLastName())
                 .setRole(RoleCode.CLIENT)
-                .setEmail("ahmed@gmail.com");
+                .setEmail(randomEmail());
     }
 
     private void given_the_person_email_is_null() {
