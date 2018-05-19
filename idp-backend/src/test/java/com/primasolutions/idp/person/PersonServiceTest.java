6494d8233f31151f8fd7fd01041d77c2b6f7dbd7
Moulay Ahmed <ahmed.elidrissi.attach@gmail.com>
2018-05-19 19:14:03 +0200

    To Prima-solutions

--- idp-backend/src/test/java/com/primasolutions/idp/person/PersonServiceTest.java
+++ idp-backend/src/test/java/com/primasolutions/idp/person/PersonServiceTest.java
@@ -12,14 +12,14 @@ import com.primasolutions.idp.person.mocks.MockPersonLookuper;
 import com.primasolutions.idp.person.mocks.MockPersonSaver;
 import com.primasolutions.idp.person.mocks.MockPersonValidator;
 import com.primasolutions.idp.tools.MockReseter;
-import com.tngtech.java.junit.dataprovider.DataProvider;
-import com.tngtech.java.junit.dataprovider.DataProviderRunner;
-import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import org.hamcrest.Matchers;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static com.primasolutions.idp.tools.RandomForTests.randomBirthdate;
 import static com.primasolutions.idp.tools.RandomForTests.randomEmail;
@@ -27,10 +27,10 @@ import static com.primasolutions.idp.tools.RandomForTests.randomString;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.test.util.ReflectionTestUtils.setField;
 
-@RunWith(DataProviderRunner.class)
-public class PersonServiceTest {
+class PersonServiceTest {
 
     private MockPersonValidator personValidator;
 
@@ -38,8 +38,8 @@ public class PersonServiceTest {
 
     private PersonBean person;
 
-    @Before
-    public void setup() {
+    @BeforeEach
+    void setup() {
         personService = new PersonService();
         personValidator = MockPersonValidator.getInstance();
         setField(personService, "personValidator", personValidator);
@@ -49,25 +49,25 @@ public class PersonServiceTest {
         setField(personService, "personLookuper", MockPersonLookuper.getInstance());
     }
 
-    @After
-    public void tearsDown() {
+    @AfterEach
+    void tearsDown() {
         MockReseter.resetAllMocks();
     }
 
-    @Test(expected = IDPException.class)
-    public void when_validation_is_ko_then_error() {
-        personService.createPerson(null);
-    }
-
-    @Test(expected = IDPException.class)
-    public void when_create_a_new_person_and_not_valid_then_error() {
-        personValidator.setForcedError(IDPException.newInstance());
-        given_new_person_to_create();
-        when_create_new_person();
+    @Test
+    void when_validation_is_ko_then_error() {
+        assertThrows(IDPException.class, () -> personService.createPerson(null));
     }
 
     @Test
-    public void when_create_a_new_valid_person_then_save_her() {
+    void when_create_a_new_person_and_not_valid_then_error() {
+        personValidator.setForcedError(IDPException.newInstance());
+        given_new_person_to_create();
+        assertThrows(IDPException.class, this::when_create_new_person);
+    }
+
+    @Test
+    void when_create_a_new_valid_person_then_save_her() {
         given_new_person_to_create();
         final PersonBean result = when_create_new_person();
         assertThat(result, notNullValue());
@@ -75,7 +75,7 @@ public class PersonServiceTest {
     }
 
     @Test
-    public void when_create_a_new_valid_person_then_save_her_auth_info() {
+    void when_create_a_new_valid_person_then_save_her_auth_info() {
         given_new_person_to_create();
         when_create_new_person();
         final AuthenticationInfo actual = MockAuthenticationInfoLookuper.getInstance().byLogin((person.getEmail()));
@@ -83,24 +83,23 @@ public class PersonServiceTest {
         assertThat(actual.getLogin(), Matchers.equalTo(person.getEmail()));
     }
 
-    @DataProvider
-    public static Object[] emailAddresses() {
+    private static Stream<String> emailAddresses() {
         // @formatter:off
-        return new Object[]{
+        return Stream.of(
                 randomEmail(),
                 randomEmail(),
                 randomEmail(),
-                randomEmail(),
-        };
+                randomEmail()
+                );
         // @formatter:on
     }
 
-    @Test(expected = IDPException.class)
-    @UseDataProvider("emailAddresses")
-    public void when_create_a_new_person_with_an_elready_existing_email_then_error(final String emailAddress) {
+    @ParameterizedTest
+    @MethodSource("emailAddresses")
+    void when_create_a_new_person_with_an_already_existing_email_then_error(final String emailAddress) {
         given_an_existing_person_with_email(emailAddress);
         given_new_person_to_create_with_email(emailAddress);
-        when_create_new_person();
+        assertThrows(IDPException.class, this::when_create_new_person);
     }
 
     private void given_an_existing_person_with_email(final String emailAddress) {
