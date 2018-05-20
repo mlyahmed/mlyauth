9e4a11a26b3bb1e9f6205d96083c5f285dd26c96
Moulay Ahmed <ahmed.elidrissi.attach@gmail.com>
2018-05-21 01:12:13 +0200

    To Prima-solutions

--- idp-backend/src/main/java/com/primasolutions/idp/person/IPersonService.java
+++ idp-backend/src/main/java/com/primasolutions/idp/person/IPersonService.java
@@ -1,9 +1,13 @@
 package com.primasolutions.idp.person;
 
 public interface IPersonService {
+
     PersonBean lookupPerson(String externalId);
+
     PersonBean createPerson(PersonBean bean);
+
     PersonBean updatePerson(PersonBean bean);
+
     void assignApplication(String appname, String personExternalId);
 
 }
