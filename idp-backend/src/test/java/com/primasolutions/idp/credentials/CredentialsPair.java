69b4991add1e813145d00004b5bcf2bbcb082172
Moulay Ahmed <ahmed.elidrissi.attach@gmail.com>
2019-02-01 16:15:38 +0100

    java 11

--- idp-backend/src/test/java/com/primasolutions/idp/credentials/CredentialsPair.java
+++ idp-backend/src/test/java/com/primasolutions/idp/credentials/CredentialsPair.java
@@ -1,20 +1,32 @@
 package com.primasolutions.idp.credentials;
 
-import lombok.Data;
-
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.cert.X509Certificate;
 
-@Data
 public class CredentialsPair {
 
     private final PrivateKey privateKey;
     private final X509Certificate certificate;
 
+    public CredentialsPair(final PrivateKey privateKey, final X509Certificate certificate) {
+        this.privateKey = privateKey;
+        this.certificate = certificate;
+    }
 
     public PublicKey getPublicKey() {
         return certificate.getPublicKey();
     }
+
+
+    public X509Certificate getCertificate() {
+        return certificate;
+    }
+
+    public PrivateKey getPrivateKey() {
+        return privateKey;
+    }
+
+
 
 }
