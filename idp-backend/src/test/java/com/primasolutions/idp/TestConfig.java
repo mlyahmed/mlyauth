823fbbb271123f129b8d1356b345f210cca8fe39
Moulay Ahmed <ahmed.elidrissi.attach@gmail.com>
2019-02-01 17:14:46 +0100

    java 11

--- idp-backend/src/test/java/com/primasolutions/idp/TestConfig.java
+++ idp-backend/src/test/java/com/primasolutions/idp/TestConfig.java
@@ -2,9 +2,14 @@ package com.primasolutions.idp;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.testcontainers.containers.MariaDBContainer;
 
 import java.security.Security;
 
@@ -13,6 +18,21 @@ import static com.primasolutions.idp.tools.ConstantsForTests.TEST_START_UP_PASSP
 @Configuration
 @Profile("test")
 public class TestConfig {
+
+    private static final MariaDBContainer DB_CONTAINER = new MariaDBContainer("mariadb:10.3.6");
+    static {
+        DB_CONTAINER.start();
+    }
+
+    static MariaDBContainer dbContainer() {
+        return DB_CONTAINER;
+    }
+
+    @Bean(destroyMethod = "stop")
+    public MariaDBContainer databaseContainer() {
+        return dbContainer();
+    }
+
 
     @Bean(name = "jasyptStringEncryptor")
     public StandardPBEStringEncryptor jasyptStringEncryptor() {
@@ -23,5 +43,34 @@ public class TestConfig {
         encryptor.setProviderName(BouncyCastleProvider.PROVIDER_NAME);
         return encryptor;
     }
+
+
+    public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        public void initialize(ConfigurableApplicationContext appConf) {
+            TestPropertyValues.of(driver(), url(), username(), password()).applyTo(appConf.getEnvironment());
+        }
+
+        @NotNull
+        private String password() {
+            return "spring.datasource.password=" + dbContainer().getPassword();
+        }
+
+        @NotNull
+        private String username() {
+            return "spring.datasource.username=" + dbContainer().getUsername();
+        }
+
+        @NotNull
+        private String url() {
+            return "spring.datasource.url=" + dbContainer().getJdbcUrl();
+        }
+
+        private String driver() {
+            return "spring.datasource.driver-class-name=" + dbContainer().getDriverClassName();
+        }
+
+    }
+
+
 
 }
