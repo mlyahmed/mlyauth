9f59a2d048d774847827cc116285b6d3f9169c40
Moulay Ahmed <ahmed.elidrissi.attach@gmail.com>
2019-02-01 20:01:09 +0100

    java 11

--- gradlew.bat
+++ gradlew.bat
@@ -15,7 +15,7 @@ set APP_HOME=%DIRNAME%
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 set GRADLE_OPTS=-Xmx2024m -XX:MaxHeapSize=256m %GRADLE_OPTS%
-set DEFAULT_JVM_OPTS=
+set DEFAULT_JVM_OPTS="-Xmx64m"
 
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
